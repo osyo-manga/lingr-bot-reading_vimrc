ef36a58220b04033934974af60bb3f02877474b6
ujihisa <ujihisa@gmail.com>
2013-05-24 16:16:19 -0700

    indentation. see also: http://shugo.net/ruby-codeconv/codeconv.html

--- reading_vimrc.rb
+++ reading_vimrc.rb
@@ -1,44 +1,44 @@
 # -*- encoding: UTF-8 -*-
 
 class ReadingVimrc
-	def initialize
-		@is_running_ = false
-		@messages = []
-	end
+  def initialize
+    @is_running_ = false
+    @messages = []
+  end
 
-	def is_running?
-		@is_running_
-	end
+  def is_running?
+    @is_running_
+  end
 
-	def start
-		@is_running_ = true
-		reset
-	end
+  def start
+    @is_running_ = true
+    reset
+  end
 
-	def stop
-		@is_running_ = false
-	end
-	
-	def reset
-		@messages = []
-	end
+  def stop
+    @is_running_ = false
+  end
 
-	def members
-		@messages.map {|mes| mes[:name] }.uniq
-	end
+  def reset
+    @messages = []
+  end
 
-	def messages
-		@messages
-	end
+  def members
+    @messages.map {|mes| mes[:name] }.uniq
+  end
 
-	def status
-		is_running? ? "started" : "stopped"
-	end
+  def messages
+    @messages
+  end
 
-	def add(message)
-		if is_running?
-			@messages << message
-		end
-	end
+  def status
+    is_running? ? "started" : "stopped"
+  end
+
+  def add(message)
+    if is_running?
+      @messages << message
+    end
+  end
 end
 
--- web.rb
+++ web.rb
@@ -7,7 +7,7 @@ load "reading_vimrc.rb"
 
 
 get '/' do
-	"Hello, world"
+  "Hello, world"
 end
 
 
@@ -29,54 +29,54 @@ EOS
 
 
 get '/reading_vimrc' do
-	"status: #{reading_vimrc.status}<br>members<br>#{reading_vimrc.members.join('<br>')}"
+  "status: #{reading_vimrc.status}<br>members<br>#{reading_vimrc.members.join('<br>')}"
 end
 
 
 post '/reading_vimrc' do
-	content_type :text
-	json = JSON.parse(request.body.string)
-	json["events"].select {|e| e['message'] }.map {|e|
-		text = e["message"]["text"]
-		if /^!reading_vimrc[\s　]start$/ =~ text
-			reading_vimrc.start
-			return "started"
-		end
-		if /^!reading_vimrc[\s　]stop$/ =~ text
-			reading_vimrc.stop
-			return "stoped"
-		end
-		if /^!reading_vimrc[\s　]status$/ =~ text
-			return reading_vimrc.status
-		end
-		if /^!reading_vimrc[\s　]member$/ =~ text
-			members = reading_vimrc.members
-			return members.empty? ? "だれもいませんでした" : members.sort.join("\n")
-		end
-		if /^!reading_vimrc[\s　]member_with_count$/ =~ text
-			names = reading_vimrc.messages.map {|mes| mes[:name] }
-			if names.empty?
-				return "だれもいませんでした"
-			end
-			return names.inject(Hash.new(0)) { 
-				|h,o| h[o]+=1; h
-			}.sort_by { |k,v| -v }.map { |name, count|
-				"#{count}回 : #{name}"
-			}.join("\n")
-		end
-		if /^!reading_vimrc[\s　]reset$/ =~ text
-			return reading_vimrc.reset
-		end
-		if /^!reading_vimrc[\s　]help$/ =~ text
-			return reading_vimrc_help
-		end
+  content_type :text
+  json = JSON.parse(request.body.string)
+  json["events"].select {|e| e['message'] }.map {|e|
+    text = e["message"]["text"]
+    if /^!reading_vimrc[\s　]start$/ =~ text
+      reading_vimrc.start
+      return "started"
+    end
+    if /^!reading_vimrc[\s　]stop$/ =~ text
+      reading_vimrc.stop
+      return "stoped"
+    end
+    if /^!reading_vimrc[\s　]status$/ =~ text
+      return reading_vimrc.status
+    end
+    if /^!reading_vimrc[\s　]member$/ =~ text
+      members = reading_vimrc.members
+      return members.empty? ? "だれもいませんでした" : members.sort.join("\n")
+    end
+    if /^!reading_vimrc[\s　]member_with_count$/ =~ text
+      names = reading_vimrc.messages.map {|mes| mes[:name] }
+      if names.empty?
+        return "だれもいませんでした"
+      end
+      return names.inject(Hash.new(0)) { 
+        |h,o| h[o]+=1; h
+      }.sort_by { |k,v| -v }.map { |name, count|
+        "#{count}回 : #{name}"
+      }.join("\n")
+    end
+    if /^!reading_vimrc[\s　]reset$/ =~ text
+      return reading_vimrc.reset
+    end
+    if /^!reading_vimrc[\s　]help$/ =~ text
+      return reading_vimrc_help
+    end
 
-		if /^!reading_vimrc[\s　]*(.+)$/ =~ text
-			return "Not found command"
-		end
-		reading_vimrc.add({:name => e["message"]["nickname"], :text => text})
-	}
-	return ""
+    if /^!reading_vimrc[\s　]*(.+)$/ =~ text
+      return "Not found command"
+    end
+    reading_vimrc.add({:name => e["message"]["nickname"], :text => text})
+  }
+  return ""
 end
 
 
