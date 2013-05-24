db335c51a02008f0d6aedae87081c441d61e0732
ujihisa <ujihisa@gmail.com>
2013-05-24 16:17:08 -0700

    the locations of colons weren't aligned

--- web.rb
+++ web.rb
@@ -18,13 +18,13 @@ vimrc読書会で発言した人を集計するための bot です
 
 !reading_vimrc {command}
 
-"start"  : 集計の開始、"member" は "reset" される
-"stop"   : 集計の終了
-"status" : ステータスの出力
-"member" : "start" ～ "stop" の間に発言した人を列挙
-"member_with_count" : "member" に発言数も追加して列挙
-"reset"  : "member" をリセット
-"help"   : 使い方を出力
+"start":   集計の開始、"member" は "reset" される
+"stop":    集計の終了
+"status":  ステータスの出力
+"member":  "start" ～ "stop" の間に発言した人を列挙
+"member_with_count":  "member" に発言数も追加して列挙
+"reset":   "member" をリセット
+"help":    使い方を出力
 EOS
 
 
