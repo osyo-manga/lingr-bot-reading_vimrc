1c266eb4967a791c86f34ce14058c36b658677c4
manga_osyo <manga.osyo@gmail.com>
2013-07-26 23:35:42 +0900

    Add owner.

--- web.rb
+++ web.rb
@@ -11,7 +11,7 @@ get '/' do
 end
 
 
-OWNER = ["manga_osyo", "thinca"]
+OWNER = ["manga_osyo", "thinca", "deris0126", "rbtnn ", "LeafCage "]
 
 def owner?(name)
 	!!OWNER.index(name)
@@ -52,24 +52,25 @@ post '/reading_vimrc' do
 	json["events"].select {|e| e['message'] }.map {|e|
 		text = e["message"]["text"]
 		name = e["message"]["nickname"]
+		speaker_id = e["message"]["speaker_id"]
 		
-		if /^=== 第\d+回 vimrc読書会 ===/ =~ text && owner?(name)
+		if /^=== 第\d+回 vimrc読書会 ===/ =~ text && owner?(speaker_id)
 			reading_vimrc.start to_lingr_link(e["message"])
 			return "started"
 		end
-		if /^!reading_vimrc[\s　]start$/ =~ text && owner?(name)
+		if /^!reading_vimrc[\s　]start$/ =~ text && owner?(speaker_id)
 			reading_vimrc.start to_lingr_link(e["message"])
 			return "started"
 		end
-		if /^!reading_vimrc[\s　]stop$/ =~ text && owner?(name)
+		if /^!reading_vimrc[\s　]stop$/ =~ text && owner?(speaker_id)
 			reading_vimrc.stop
 			return "stoped"
 		end
-		if /^!reading_vimrc[\s　]reset$/ =~ text && owner?(name)
+		if /^!reading_vimrc[\s　]reset$/ =~ text && owner?(speaker_id)
 			reading_vimrc.reset
 			return "reset"
 		end
-		if /^!reading_vimrc[\s　]restore$/ =~ text && owner?(name)
+		if /^!reading_vimrc[\s　]restore$/ =~ text && owner?(speaker_id)
 			reading_vimrc.restore
 			return "restore"
 		end
