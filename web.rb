ca1372d12d0ed1b44bbb4db250c176af1fb1fdc3
manga_osyo <manga.osyo@gmail.com>
2014-01-12 02:36:56 +0900

    Add yml.

--- web.rb
+++ web.rb
@@ -48,6 +48,44 @@ def to_lingr_link(message)
 end
 
 
+get '/reading_vimrc/yml' do
+	log = reading_vimrc.start_link
+	member = reading_vimrc.members.sort
+	yml = <<"EOS"
+- id: 1
+  date: 2013-12-14 21:00
+  date: #{ Time.now.strftime("%Y-%m-%d") } 21:00
+  day: Sat
+  plugins:
+    - name: junkfile.vim
+      author: Shougo
+      url: https://github.com/Shougo/junkfile.vim
+      hash: 13384f507ea515af84d0f138a48c53378c7b30f3
+    - name: vim-visualstar
+      author: thinca
+      url: https://github.com/thinca/vim-visualstar
+      hash: 04327d52ee3b9ad35d1f6ac8919a6365352b2262
+  members:
+#{ member.map{ |m| "    - " + m }.join("\n") }
+    - DeaR
+    - cohama
+    - deris0126
+    - haya14busa
+    - itchyny
+    - lisia
+    - manga_osyo
+    - msmhrt
+    - raa0121
+    - supermomonga
+    - syui
+    - thinca
+    - zoncoen
+  log: #{ log }
+  links:
+EOS
+	yml.gsub(/\n/, "<br>")
+end
+
 post '/reading_vimrc' do
 	content_type :text
 	json = JSON.parse(request.body.string)
